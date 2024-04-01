eb9bc1e2ef1eb473d1343f7e5cd7bb0684845b2b
Drak0Blu3 <luisanchezmolina2@gmail.com>
2024-04-01 12:59:56 -0400

    add mason ensure install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -12,6 +12,11 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "clangd",
+        "cssls",
+        "emmet_language_server",
+        "jsonls",
+        "tsserver",
         -- add more arguments for adding more language servers
       })
     end,
@@ -23,10 +28,15 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+        "prettierd",
         "stylua",
+        "biome",
         -- add more arguments for adding more null-ls sources
-      })
+      }),
+      require("lspconfig").biome.setup {
+        single_file_support = true,
+        settings = {},
+      }
     end,
   },
   {
@@ -35,7 +45,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
+        -- "python",
+        "cpptools",
         -- add more arguments for adding more debuggers
       })
     end,
